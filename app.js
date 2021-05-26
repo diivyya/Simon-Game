62ed5b35720acf3f4e13361a0f2a72f6c044247e
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-05-26 22:16:54 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const h = document.querySelector('h3');
+const b1 = document.querySelector('#0');
+const b2 = document.querySelector('#1');
+const b3 = document.querySelector('#2');
+const b4 = document.querySelector('#3');
+
+let arr = [];
+
+
+
